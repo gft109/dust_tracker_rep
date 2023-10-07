4c09af37861c60b3edc4eccd155f0485ea094555
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-10-07 13:17:50 -0300

    estrutura do app
    feita com flask, html home no jeito

---
+++ .venv/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify, json, redirect, make_response, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def login():
+    return render_template('home.html')
+    
+
+@app.route("/emit")
+def home():
+    return render_template('emit.html')
+
+@app.route("/dust")
+def dust():
+    return render_template('dust.html')
+
+@app.route("/aboutus")
+def about():
+    return render_template('aboutus.html')
+
+if __name__== "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dust_tracker_rep
  Nasa project rep
+
+TUTORIAL 
+
+- Selecione o Python Interpreter recomendado (ctrl + shift + p)
+- Crie um venv (ambiente virtual de desenvolvimento)
+- Venv: consulte o repositório estudos python para saber como criar
+- Após isso: de o comando no terminal: pip install -r requirements.txt (para instalar todas as bibliotecas necessárias)
+- Depois disso, abra um novo terminal e digite: flask run 
